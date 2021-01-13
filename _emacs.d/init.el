4d575efc02225f72554fb81ccc8abb25d5f7db54
Dave Churchill <dave@ninthcuriosity.com>
2021-01-13 22:32:33 +1100

    Emacs: Switch to straight.el

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -1,15 +1,22 @@
 ;;; -*- lexical-binding: t -*-
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
+(setq straight-use-package-by-default t)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
+(straight-use-package 'use-package)
 (eval-when-compile (require 'use-package))
-(setq use-package-always-ensure t)
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
