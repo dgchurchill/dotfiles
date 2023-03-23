0a64a4380dc20b391758f6ea93fcb09450a8d036
Dave Churchill <dave@ninthcuriosity.com>
2023-03-24 08:41:52 +1100

    Emacs: bankrupt, fresh start

---
+++ _emacs.d/custom.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("896e4305e7c10f3217c5c0a0ef9d99240c3342414ec5bfca4ec4bff27abe2d2d" "bfc0b9c3de0382e452a878a1fb4726e1302bf9da20e69d6ec1cd1d5d82f61e3d" "cfe4d36ed4cf00a541f7ba0deb38c94808c13a3e4c717f07bc3b9c866670e8d1" default))
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(modus-themes-headings
+   '((0 variable-pitch 2.0)
+     (1 variable-pitch 1.8)
+     (2 variable-pitch 1.6)
+     (3 variable-pitch 1.4)
+     (4 variable-pitch 1.2)
+     (5 variable-pitch)))
+ '(modus-themes-mixed-fonts t)
+ '(straight-vc-git-default-clone-depth 1)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ _emacs.d/init.el
@@ -0,0 +1,151 @@
+;;; Set up straight and use-package
+
+(setq straight-use-package-by-default t)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(eval-when-compile (require 'use-package))
+
+
+;;; Core settings
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+(server-start)
+
+(setq enable-recursive-minibuffers t)
+(minibuffer-depth-indicate-mode)
+
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :init
+  (exec-path-from-shell-initialize))
+
+;; Set up escape as a leader
+;;
+;; <escape> is the escape key when in a window system. It's normally translated to ESC (^[) by
+;; function-key-map, but not if we explicitly bind it.  Don't bind to ESC, because that's where all
+;; the M- keybindings actually live (via the meta-prefix-char variable).
+(setq escape-map (make-sparse-keymap))
+(global-set-key (kbd "<escape>") escape-map)
+
+
+;;; Themes and appearance
+
+(use-package modus-themes
+  :config
+  (load-theme 'modus-operandi-tinted))
+
+(set-face-attribute 'default nil :family "Iosevka Slab" :height 140 :weight 'light)
+(set-face-attribute 'fixed-pitch nil :family "Iosevka Slab" :height 1.0 :weight 'light)
+(set-face-attribute 'variable-pitch nil :family "Iosevka Etoile" :height 1.0 :weight 'light)
+
+
+;;; Help
+
+(use-package which-key
+  :init
+  (which-key-mode))
+
+
+;;; Completion
+
+(use-package vertico
+  :init
+  (vertico-mode))
+
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
+
+(use-package marginalia
+  :bind (:map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+  :init
+  (marginalia-mode))
+
+(use-package consult
+  :bind (;; C-c bindings (mode-specific-map)
+	 ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings (ctl-x-map)
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings (goto-map)
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings (search-map)
+         ("M-s d" . consult-find)
+         ("M-s D" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+)
+
+(use-package recentf
+  :init
+  (recentf-mode))
+
+
+;;; Org mode
+
+(use-package org
+  :init
+  (require 'org-protocol)
+  (add-hook 'org-mode-hook 'variable-pitch-mode)
+  (add-hook 'org-mode-hook 'visual-line-mode))
+
+
+;;; Magit
+
+(use-package magit)
